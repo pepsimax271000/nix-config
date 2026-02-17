05e9a46fc2c33922cad56b64ed315dbc9b7bbbcb
pepsimax271000 <248238336+pepsimax271000@users.noreply.github.com>
2026-02-17 01:36:47 +0000

    update names

--- modules/home/gui/hyprland/source/monitors.nix
+++ modules/home/gui/hyprland/source/monitors.nix
@@ -2,10 +2,10 @@
 {
   wayland.windowManager.hyprland = {
     extraConfig = "
-      monitor=DP-1,3440x1440@165.0,1920x167,1.0
-      monitor=DP-2,1920x1200@59.95,5360x0,1.0
-      monitor=DP-2,transform,1
-      monitor=DP-3,1920x1080@60.0,0x293,1.0
+      monitor=DP-1,3440x1440@165.0,1270x1080,1.0
+      monitor=DP-2,1920x1080@120.0,1963x0,1.0
+      monitor=DP-3,1920x1200@60.0,70x787,1.0
+      monitor=DP-3,transform,3
       monitor=eDP-1,3840x2160@60,auto,1.5
       monitor=LVDS-1,1920x1080@60,auto,1
     ";
